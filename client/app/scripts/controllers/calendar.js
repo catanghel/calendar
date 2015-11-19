3b5d6c840c7634ee3ca6cd61ea5b95a0a224d547
catalin.anghel <catalin.anghel@iquestgroup.com>
2015-11-19 16:49:15 +0200

    calendar list on the left

--- client/app/scripts/controllers/calendar.js
+++ client/app/scripts/controllers/calendar.js
@@ -8,13 +8,18 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('CalendarCtrl', function ($scope, CalendarEvent) {
+  .controller('CalendarCtrl', function ($scope, Calendar, CalendarEvent) {
     var i, event, day;
 
     // just testing restangular and node-restful with parameters
+    $scope.calendars = [];
     $scope.events = [];
-    //$scope.currentMonth = $scope.month.month();
-    $scope.calendar = '564d8f861e3dcf101db271af';
+    $scope.calendar = '';
+
+    $scope.onCalendarSelect = function (id) {
+      $scope.calendar = id;
+      CalendarEvent.getList({calendar: $scope.calendar}).then($scope.populate);
+    };
 
     $scope.populate = function(events) {
       var eventsThisMonth;
@@ -23,19 +28,23 @@ angular.module('clientApp')
       }
 
       eventsThisMonth = _.filter($scope.events, function(e) {
-        console.log(e);
         return e.start.month === $scope.month.month() && e.start.year === $scope.month.year();
       });
 
       _.each(eventsThisMonth, function(e, k) {
         if (e) {
           day = $('[data-day=' + e.start.day + ']').find('.events-container');
+          day.empty();
           day.append('<li class="event">' + e.title + '</li>');
         }
       });
+    };
+
+    function getCalendars(calendars) {
+      $scope.calendars = calendars;
+      $scope.calendar = $scope.calendars[0]._id;
+      CalendarEvent.getList({calendar: $scope.calendar}).then($scope.populate);
     }
 
-    CalendarEvent.getList({calendar: $scope.calendar}).then($scope.populate);
-
-
+    Calendar.getList().then(getCalendars);
   });
